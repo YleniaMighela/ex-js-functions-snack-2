4f9e07f74960a6c848047d32fa150fa751f30189
Ylenia <yleniamighela@gmail.com>
2025-04-23 12:06:50 +0200

    snack02

---
+++ snack2.js
@@ -0,0 +1,8 @@
+// Crea una arrow function che calcola il quadrato di un numero.
+
+// Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+
+const quadrato = (numero) => numero * numero;
+
+console.log(quadrato(2));
+
