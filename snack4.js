32cfbfc652536c5e135c6e846d4cb33c3600e5ac
Ylenia <yleniamighela@gmail.com>
2025-04-23 12:28:14 +0200

    snack04

---
+++ snack4.js
@@ -0,0 +1,13 @@
+// Crea un generatore di funzioni creaTimer
+// Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
+
+function creaTimer(tempo) {
+    return () => {
+        setTimeout(() => {
+            console.log("Tempo scaduto!");
+        }, tempo);
+    }
+};
+
+const timer = creaTimer(1000);
+timer();
