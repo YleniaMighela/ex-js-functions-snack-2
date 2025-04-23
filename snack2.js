86cddb28b7fdaf255bf4b25c219b34e0c04e0ac6
Ylenia <yleniamighela@gmail.com>
2025-04-23 12:08:32 +0200

    tolte parentesi tonde

--- snack2.js
+++ snack2.js
@@ -2,7 +2,7 @@
 
 // Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
 
-const quadrato = (numero) => numero * numero;
+const quadrato = numero => numero * numero;
 
 console.log(quadrato(2));
 
